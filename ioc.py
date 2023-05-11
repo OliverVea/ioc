a8b087e83a16534553b22e2e00eaa79553b4ac05
LTOLVE01\olve <olve@bizzkit.com>
2023-05-11 20:45:18 +0900

    adding clear_previous

--- ioc.py
+++ ioc.py
@@ -5,7 +5,7 @@ from ioc.errors import *
 class IOC:
     def __init__(self):
         self._anonymous_instances: dict[int, list[object]] = {}
-        self._services: dict[int, list[object]] = {}
+        self._named_instances: dict[int, list[object]] = {}
 
     def _anonymous_instance_key(self, instance_type: Type) -> int:
         return (instance_type)
@@ -13,7 +13,7 @@ class IOC:
     def _named_instance_key(self, instance_type: Type, instance_id: str) -> int:
         return (instance_type, instance_id)
         
-    def add_instance(self, instance: Any, instance_type: Type | None = None, instance_id: str | None = None) -> None:
+    def add_instance(self, instance: Any, instance_type: Type | None = None, instance_id: str | None = None, clear_previous: bool = False) -> None:
         actual_type = type(instance)
 
         if instance_type:
@@ -26,25 +26,27 @@ class IOC:
 
         if instance_id:
             service_key = self._named_instance_key(instance_type, instance_id)
-            services = self._services.setdefault(service_key, [])
-            
-            if instance not in services:
-                services.append(instance)
-
+            collection = self._named_instances
+        
         else:
             service_key = self._anonymous_instance_key(instance_type)
-            services = self._anonymous_instances.setdefault(service_key, [])
-            
-            if instance not in services:
-                services.append(instance)
+            collection = self._anonymous_instances
+
+        if clear_previous and service_key in collection:
+            del collection[service_key]
+
+        services = collection.setdefault(service_key, [])
+        
+        if instance not in services:
+            services.append(instance)
 
     def get_instance(self, instance_type: Type, instance_id: str | None = None, required: bool = False) -> Any:
         service = None
 
         if instance_id: 
             instance_key = self._named_instance_key(instance_type, instance_id)
-            if instance_key in self._services:
-                service = self._services[instance_key][-1]
+            if instance_key in self._named_instances:
+                service = self._named_instances[instance_key][-1]
 
         else:
             instance_key = self._anonymous_instance_key(instance_type)
